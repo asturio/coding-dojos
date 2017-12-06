0b94d09bce584ed6c1ba9cc4da3f398d257253ff
Claudio Clemens <asturio@gmx.net>
2017-12-06 23:41:40 +0100

    IDEA settings
    Only Reverse, if first condition is ok.

--- mother-java/src/main/java/bogus/karameikos/motherjava/MotherJavaTester.java
+++ mother-java/src/main/java/bogus/karameikos/motherjava/MotherJavaTester.java
@@ -4,14 +4,26 @@ public class MotherJavaTester {
     public static void main(String... args) {
         MotherJava mother = new MotherJava();
 
-        for (long eingabe = 987654321; eingabe > 0; eingabe--) {
-            String eingabeStr = Long.toString(eingabe);
-            String reverse = mother.reverse(eingabe+"");
+        // option 1: The combination must be a valid 9-digit number (no leading zeros): 102345678
+        // option 2: The combination must not be a valid 9-digit number (leading zeros are ok): 012345678
+        long lowestCombination = 12345678; // as Code 012345678
+        long highestCombination = 987654321; // there is only this option
 
-            if (mother.checkLength(eingabeStr) && mother.isSquare(Long.parseLong(reverse))) {
-                System.out.println("Zahl gefunden: " + eingabeStr + ", " + reverse + " ist ²");
-                break;
+        long start = System.nanoTime();
+        int count = 0;
+        for (long eingabe = highestCombination; eingabe >= lowestCombination; eingabe--) {
+            String eingabeStr = Long.toString(eingabe);
+            ++count;
+            if (mother.checkLength(eingabeStr)) {
+                // only do extra work, if the first condition is passed
+                String reverse = mother.reverse(eingabe+"");
+                if (mother.isSquare(Long.parseLong(reverse))) {
+                    System.out.println("Zahl gefunden: " + eingabeStr + ", reverse: " + reverse + " ist quadrat");
+                    break;
+                }
             }
         }
+        System.out.println("Terminated in " + ((System.nanoTime() - start) / 1E6) + " ms." +
+                " After " + count + " iterations.");
     }
 }
